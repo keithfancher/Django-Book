4af24122340e71b3abf6ed11fa6aa4eb824e3265
ktf <not.discostoo@gmail.com>
2011-07-28 11:37:42 -0700

    through Ch.7

--- urls.py
+++ urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import *
 from mysite.views import hello, current_datetime, hours_ahead
 from mysite.books import views
+from mysite.contact.views import contact, contact_thanks
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -12,4 +13,6 @@ urlpatterns = patterns('',
     (r'^time/plus/(\d{1,2})$', hours_ahead),
     (r'^admin/', include(admin.site.urls)),
     (r'^search/$', views.search),
+    (r'^contact/$', contact),
+    (r'^contact/thanks/$', contact_thanks),
 )
